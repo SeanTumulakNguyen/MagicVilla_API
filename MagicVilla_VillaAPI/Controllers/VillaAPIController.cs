8ee7c97f7755a5666ac7b7bcbab5d8bcb1d6fe51
Sean Nguyen <sean.nguyenf@yahoo.com>
2023-03-27 20:03:09 -0400

    introduce status code responses

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -14,16 +14,25 @@ namespace MagicVilla_VillaAPI.Controllers
     public class VillaAPIController : ControllerBase
     {
         [HttpGet]
-        public IEnumerable<VillaDto> GetVillas()
+        public ActionResult<IEnumerable<VillaDto>> GetVillas()
         {
-            return VillaStore.villaList;
+            return Ok(VillaStore.villaList);
         }
 
         // if you do not define HTTP Verb, it defaults to HTTPGET
         [HttpGet("{id:int}")]
-        public VillaDto? GetVilla(int id)
+        public ActionResult<VillaDto?> GetVilla(int id)
         {
-            return VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+            var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+            if (villa == null)
+            {
+                return NotFound(); 
+            }
+            return Ok(villa);
         }
     }
 }
