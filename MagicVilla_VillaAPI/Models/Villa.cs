162487e74ffcde6bed3358ea126db77576c55116
Sean Nguyen <sean.nguyenf@yahoo.com>
2023-05-09 20:11:14 -0400

    new fields to villa dto and villa classes

--- MagicVilla_VillaAPI/Models/Villa.cs
+++ MagicVilla_VillaAPI/Models/Villa.cs
@@ -4,6 +4,11 @@
     {
         public int Id { get; set; }
         public string? Name { get; set; }
+        public int Sqft { get; set; }
+        public int Occupancy { get; set; }
+        public string ImageUrl { get; set; }
+        public string Amenity { get; set; }
         public DateTime? CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
     }
 }
